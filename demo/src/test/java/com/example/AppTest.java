3570905f29456a8f564f081c3c4d9c97bd288ddb
Lucas Monteiro <lucas.monteiro.soares13@gmail.com>
2025-10-03 20:29:43 +0000

    Teste integração Jenkins

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -1,20 +1,13 @@
 package com.example;
 
+import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testApp() {
+        // Teste simples para validar o build
+        assertTrue(true);
     }
 }
