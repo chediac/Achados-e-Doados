750951c7bd5e6c1de5f754ee32990fc2dd149756
Lucas Monteiro <lucas.monteiro.soares13@gmail.com>
2025-10-17 13:20:24 +0000

    Estruturação inicial do projeto, instalação de dependências etc

---
+++ backend/src/main/java/com/mackenzie/achadosdoados/AchadosEDoadosApplication.java
@@ -0,0 +1,13 @@
+package com.mackenzie.achadosdoados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchadosEDoadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchadosEDoadosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mackenzie/achadosdoados/AchadosEDoadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mackenzie.achadosdoados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchadosEDoadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
