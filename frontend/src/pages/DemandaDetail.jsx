c6a132f182f34e32617a287f8cdf271366e5669c
magalhaes210804 <143047018+magalhaes210804@users.noreply.github.com>
2025-11-24 01:31:19 +0000

    teste automatizado 5.0

--- frontend/src/pages/DemandaDetail.jsx
+++ frontend/src/pages/DemandaDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { getUser, getToken } from '../lib/auth';
